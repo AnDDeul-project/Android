5e6147b0816dba473128b2b21df62dd9c5b07cc8
Jieun <lje2000lje@naver.com>
2024-07-13 03:57:54 +0900

    :sparkles: Add 로고 없는 토스트바 함수 추가
    Related to : #229

---
+++ app/src/main/java/com/umc/anddeul/common/AnddeulNoLogoToast.kt
@@ -0,0 +1,23 @@
+package com.umc.anddeul.common
+
+import android.content.Context
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.umc.anddeul.databinding.ToastNoLogoBinding
+import com.umc.anddeul.ext.toPx
+
+object AnddeulNoLogoToast {
+    fun createNoLogoToast(context: Context, message: String): Toast {
+        val inflater = LayoutInflater.from(context)
+        val binding : ToastNoLogoBinding = ToastNoLogoBinding.inflate(inflater)
+
+        binding.toastNoLogoTv.text = message
+
+        return Toast(context).apply {
+            setGravity(Gravity.BOTTOM or Gravity.CENTER, 0, 104.toPx())
+            duration = Toast.LENGTH_LONG
+            view = binding.root
+        }
+    }
+}
