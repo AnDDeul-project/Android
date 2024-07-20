15102f9661546d00a9ff078690a9981c52f5dc30
Jieun <lje2000lje@naver.com>
2024-07-20 15:25:10 +0900

    :alien: Update Delete 어노테이션 Post로 수정
    Related to : #252

--- app/src/main/java/com/umc/anddeul/mypage/network/LeaveInterface.kt
+++ app/src/main/java/com/umc/anddeul/mypage/network/LeaveInterface.kt
@@ -3,10 +3,10 @@ package com.umc.anddeul.mypage.network
 import com.umc.anddeul.mypage.model.LeaveDTO
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.DELETE
+import retrofit2.http.POST
 
 interface LeaveInterface {
-    @DELETE("/auth/kakao/unlink")
+    @POST("/auth/kakao/unlink")
     fun leaveApp(
         @Body content: String
     ): Call<LeaveDTO>
