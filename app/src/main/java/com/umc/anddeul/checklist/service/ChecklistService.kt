4c4e474981967adcb88d58f0eda1946f4459d548
Jieun <lje2000lje@naver.com>
2024-07-26 02:45:44 +0900

    :recycle: Update imgApi 구조 수정
    checkId와 uri를 받도록 수정 Related to : #294

--- app/src/main/java/com/umc/anddeul/checklist/service/ChecklistService.kt
+++ app/src/main/java/com/umc/anddeul/checklist/service/ChecklistService.kt
@@ -1,7 +1,10 @@
 package com.umc.anddeul.checklist.service
 
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
+import android.provider.MediaStore
 import android.util.Log
 import com.umc.anddeul.checklist.ChecklistRVAdapter
 import com.umc.anddeul.checklist.model.CheckImg
@@ -24,8 +27,14 @@ import retrofit2.Response
 import java.io.File
 
 class ChecklistService(context : Context) {
-    val checklistRVAdapter = ChecklistRVAdapter(context)
-    val contextServcie = context
+    private val checklistRVAdapter: ChecklistRVAdapter
+
+    init {
+        checklistRVAdapter = ChecklistRVAdapter(context) {
+        }
+    }
+
+    val contextService = context
 
     val retrofit = RetrofitManager.getRetrofitInstance()
     val service = retrofit.create(ChecklistInterface::class.java)
@@ -33,11 +42,24 @@ class ChecklistService(context : Context) {
     val spfMyId = context.getSharedPreferences("myIdSpf", Context.MODE_PRIVATE)
     val myId = spfMyId.getString("myId", "")
 
-    fun imgApi(checklist: Checklist, file: File) {
-        val checkId = checklist.check_idx
+    private fun getFileFromUri(context: Context, uri: Uri): File {
+        val contentResolver: ContentResolver = context.contentResolver
+        val projection = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(uri, projection, null, null, null)
+        cursor?.use {
+            val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+            it.moveToFirst()
+            val filePath = it.getString(columnIndex)
+            return File(filePath)
+        }
 
-        val imageFileRequestBody = RequestBody.create("image/*".toMediaTypeOrNull(), file)
-        val imagePart = MultipartBody.Part.createFormData("image", file.name, imageFileRequestBody)
+        throw IllegalArgumentException("Invalid URI")
+    }
+
+    fun imgApi(checkId: Int, uri: Uri) {
+        val selectedImg = getFileFromUri(contextService, uri)
+        val imageFileRequestBody = RequestBody.create("image/*".toMediaTypeOrNull(), selectedImg)
+        val imagePart = MultipartBody.Part.createFormData("image", selectedImg.name, imageFileRequestBody)
         val checkidRequestBody = RequestBody.create("text/plain".toMediaTypeOrNull(), checkId.toString())
 
         val imgCall : Call<CheckImgRoot> = service.imgPic(
@@ -64,7 +86,7 @@ class ChecklistService(context : Context) {
 
             override fun onFailure(call: Call<CheckImgRoot>, t: Throwable) {
                 Log.d("Checklist ImgService Fail", "imgCall: ${t.message}")
-                AnddeulErrorToast.createToast(contextServcie, "서버 연결이 불안정합니다")?.show()
+                AnddeulErrorToast.createToast(contextService, "서버 연결이 불안정합니다").show()
             }
         })
     }
@@ -90,18 +112,18 @@ class ChecklistService(context : Context) {
                     }
 
                     if (response.code() == 500) {
-                        AnddeulErrorToast.createToast(contextServcie, "인터넷 연결이 불안정합니다")?.show()
+                        AnddeulErrorToast.createToast(contextService, "인터넷 연결이 불안정합니다").show()
                     }
 
                     if(response.code() == 401) {
-                        val startIntent = Intent(contextServcie, StartActivity::class.java)
-                        contextServcie!!.startActivity(startIntent)
+                        val startIntent = Intent(contextService, StartActivity::class.java)
+                        contextService!!.startActivity(startIntent)
                     }
                 }
             }
             override fun onFailure(call: Call<CompleteRoot>, t: Throwable) {
                 Log.d("Checklist CompleteService Fail", "completeCall : ${t.message}")
-                AnddeulErrorToast.createToast(contextServcie, "서버 연결이 불안정합니다")?.show()
+                AnddeulErrorToast.createToast(contextService, "서버 연결이 불안정합니다").show()
             }
         })
     }
@@ -132,22 +154,22 @@ class ChecklistService(context : Context) {
                     }
                 }
                 if (response.code() == 500) {
-                    AnddeulErrorToast.createToast(contextServcie, "인터넷 연결이 불안정합니다").show()
+                    AnddeulErrorToast.createToast(contextService, "인터넷 연결이 불안정합니다").show()
                 }
 
                 if (response.code() == 451) {
-                    AnddeulToast.createToast(contextServcie, "해당 날짜에 만들어진 체크리스트가 없습니다.").show()
+                    AnddeulToast.createToast(contextService, "해당 날짜에 만들어진 체크리스트가 없습니다").show()
                 }
 
                 if(response.code() == 401) {
-                    val startIntent = Intent(contextServcie, StartActivity::class.java)
-                    contextServcie!!.startActivity(startIntent)
+                    val startIntent = Intent(contextService, StartActivity::class.java)
+                    contextService!!.startActivity(startIntent)
                 }
             }
 
             override fun onFailure(call: Call<Root>, t: Throwable) {
                 Log.d("Checklist ReadService Fail", "readCall: ${t.message}")
-                AnddeulErrorToast.createToast(contextServcie, "서버 연결이 불안정합니다")?.show()
+                AnddeulErrorToast.createToast(contextService, "서버 연결이 불안정합니다").show()
             }
         })
     }
